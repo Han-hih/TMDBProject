20b522a4a68f8b43ec67e2981f67bc3c86a56441
한 <hih458@naver.com>
2023-08-14 00:26:49 +0900

    reusableViewProtocol 추가

---
+++ TMDBProject/ReusableViewProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  ReusableViewProtocol.swift
+//  TMDBProject
+//
+//  Created by 황인호 on 2023/08/14.
+//
+
+
+import UIKit
+
+protocol ReusableViewProtocol {
+    static var identifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
