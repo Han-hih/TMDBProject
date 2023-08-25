58f4aed4e5d6a8bf69864d84c4bc03aec508b898
한 <hih458@naver.com>
2023-08-25 21:34:58 +0900

    앱 처음 실행 시 pageViewController가 뜨도록 설정

---
+++ TMDBProject/PageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PageViewController.swift
+//  TMDBProject
+//
+//  Created by 황인호 on 2023/08/25.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
--- TMDBProject/SceneDelegate.swift
+++ TMDBProject/SceneDelegate.swift
@@ -13,10 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let isLuanched = UserDefaults.standard.bool(forKey: "isLaunched")
+        print(isLuanched)
+        if isLuanched == false {
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            guard let vc = sb.instantiateViewController(withIdentifier: PageViewController.identifier) as?
+                    PageViewController else { return }
+            let nav = UINavigationController(rootViewController: vc)
+            window?.rootViewController = nav
+        } else {
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            guard let vc = sb.instantiateViewController(withIdentifier: ViewController.identifier) as? ViewController else { return }
+            let nav = UINavigationController(rootViewController: vc)
+            window?.rootViewController = nav
+        }
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
